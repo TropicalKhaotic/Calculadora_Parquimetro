b83d1fbe810b0860de23f0ff2b1d5d19da89bc56
Rafael <rafavs09@gmail.com>
2024-09-20 08:42:49 -0300

    Fix problem with hour insert

--- Calculadora.c
+++ Calculadora.c
@@ -4,106 +4,122 @@
     int main(){
 
         float saldo_retornavel, calculo, calculo2;
-        int horas_entrada, minutos_entrada, horas_saida, minutos_saidas, soma_entrada, soma_saida, horario;
+        int horas_entrada, horas_entrada_2, horas_saida, horas_saida_2, soma_entrada, soma_saida, horario;
         int tipo_calculadora;
         printf("tipo de calculadora\n 1 - carro_azul\n 2 - moto_azul\n 3 - carro_verde\n 4 - moto_verde\n");
         scanf("%d", &tipo_calculadora);
 
         switch (tipo_calculadora){
         case 1:
+        
             printf("Insira o valor do saldo final retornado\n");
             scanf("%f", &saldo_retornavel);
 
             printf("Insira o horario de entrada da placa\n");
             printf("Horas: \n");
             scanf("%d", &horas_entrada);
-            printf("Minutos: \n");
-            scanf("%d", &minutos_entrada);
 
             printf("Insira o horario de finalização da placa\n");
             printf("Horas: \n");
             scanf("%d", &horas_saida);
-            printf("Minutos: \n");
-            scanf("%d", &minutos_saidas);
 
-            soma_entrada = (horas_entrada * 60) + minutos_entrada;
-            soma_saida = (horas_saida * 60) + minutos_saidas;
+            horas_entrada_2 = horas_entrada % 100;
+            horas_entrada = horas_entrada  / 100;
+            soma_entrada = (horas_entrada * 60) + horas_entrada_2;
+
+            horas_saida_2 = horas_saida % 100;
+            horas_saida = horas_saida  / 100;
+            soma_saida = (horas_saida * 60) + horas_saida_2;
+
             horario = (soma_saida - soma_entrada);
             calculo = ( 4.0 / 120.0) * horario;
             calculo2 = calculo + saldo_retornavel;
+
             break;
-        
+
         case 2:
+
            printf("Insira o valor do saldo final retornado\n");
             scanf("%f", &saldo_retornavel);
 
             printf("Insira o horario de entrada da placa\n");
             printf("Horas: \n");
             scanf("%d", &horas_entrada);
-            printf("Minutos: \n");
-            scanf("%d", &minutos_entrada);
 
             printf("Insira o horario de finalização da placa\n");
             printf("Horas: \n");
             scanf("%d", &horas_saida);
-            printf("Minutos: \n");
-            scanf("%d", &minutos_saidas);
 
-            soma_entrada = (horas_entrada * 60) + minutos_entrada;
-            soma_saida = (horas_saida * 60) + minutos_saidas;
+            horas_entrada_2 = horas_entrada % 100;
+            horas_entrada = horas_entrada  / 100;
+            soma_entrada = (horas_entrada * 60) + horas_entrada_2;
+
+            horas_saida_2 = horas_saida % 100;
+            horas_saida = horas_saida  / 100;
+            soma_saida = (horas_saida * 60) + horas_saida_2;
+
             horario = (soma_saida - soma_entrada);
-            calculo = ( 3.60 / 120.0) * horario;
+            calculo = ( 4.0 / 120.0) * horario;
             calculo2 = calculo + saldo_retornavel;
+
             break;
 
         case 3:
-           printf("Insira o valor do saldo final retornado\n");
+            printf("Insira o valor do saldo final retornado\n");
             scanf("%f", &saldo_retornavel);
 
             printf("Insira o horario de entrada da placa\n");
             printf("Horas: \n");
             scanf("%d", &horas_entrada);
-            printf("Minutos: \n");
-            scanf("%d", &minutos_entrada);
 
             printf("Insira o horario de finalização da placa\n");
             printf("Horas: \n");
             scanf("%d", &horas_saida);
-            printf("Minutos: \n");
-            scanf("%d", &minutos_saidas);
 
-            soma_entrada = (horas_entrada * 60) + minutos_entrada;
-            soma_saida = (horas_saida * 60) + minutos_saidas;
+            horas_entrada_2 = horas_entrada % 100;
+            horas_entrada = horas_entrada  / 100;
+            soma_entrada = (horas_entrada * 60) + horas_entrada_2;
+
+            horas_saida_2 = horas_saida % 100;
+            horas_saida = horas_saida  / 100;
+            soma_saida = (horas_saida * 60) + horas_saida_2;
+
             horario = (soma_saida - soma_entrada);
-            calculo = ( 3.00 / 120.0) * horario;
+            calculo = ( 4.0 / 120.0) * horario;
             calculo2 = calculo + saldo_retornavel;
+
+
             break;
 
         case 4:
+
            printf("Insira o valor do saldo final retornado\n");
             scanf("%f", &saldo_retornavel);
 
             printf("Insira o horario de entrada da placa\n");
             printf("Horas: \n");
             scanf("%d", &horas_entrada);
-            printf("Minutos: \n");
-            scanf("%d", &minutos_entrada);
 
             printf("Insira o horario de finalização da placa\n");
             printf("Horas: \n");
             scanf("%d", &horas_saida);
-            printf("Minutos: \n");
-            scanf("%d", &minutos_saidas);
 
-            soma_entrada = (horas_entrada * 60) + minutos_entrada;
-            soma_saida = (horas_saida * 60) + minutos_saidas;
+            horas_entrada_2 = horas_entrada % 100;
+            horas_entrada = horas_entrada  / 100;
+            soma_entrada = (horas_entrada * 60) + horas_entrada_2;
+
+            horas_saida_2 = horas_saida % 100;
+            horas_saida = horas_saida  / 100;
+            soma_saida = (horas_saida * 60) + horas_saida_2;
+
             horario = (soma_saida - soma_entrada);
-            calculo = ( 3.60 / 120.0) * horario;
+            calculo = ( 4.0 / 120.0) * horario;
             calculo2 = calculo + saldo_retornavel;
+
             break;
         }
 
-        printf("\n Valor do saldo retornavel: %f\n",saldo_retornavel);
+        printf("\n Valor do saldo retornavel: %.2f\n",saldo_retornavel);
         printf(" O tempo de estacionamento é: %d minutos\n O valor gasto é: %.2f\n O valor da soma do estorno e valor gasto: %.2f", horario, calculo, calculo2);
         return 0;
     }
